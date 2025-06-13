6e9c6936763b757425a7dab988bbecbfb2be1eaa
rofi222 <m.rofi0604@gmail.com>
2025-06-13 18:56:01 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.title("Selamat datang di Website Informatika")
-st.image("IMG_2025-04-20-17-29-29-053.jpg",width=200)
+st.image("IMG-20250612-WA0017.jpg",width=200)
 st.write("\n")
 st.subheader("Rofi hebat")
 st.write("RAWRRR")
